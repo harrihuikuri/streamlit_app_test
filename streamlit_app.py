22f16a6936a137cb7717a570f8be8b7af0570378
Harri Huikuri <111337216+harrihuikuri@users.noreply.github.com>
2022-08-17 09:43:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,4 +70,4 @@ if streamlit.button('Add a Fruit to the List'):
     sreamlit.text(back_from_function)
 
 # This will not work correctly, but just go with it for now
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
